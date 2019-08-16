dc80c328b2807bd6c9d0af4d32f1d6a894bff4cd
Benjamin Laib <benjaminlaib@techteamer.com>
2019-08-16 13:58:08 +0200

    Removed accidental comment

--- test/test.js
+++ test/test.js
@@ -23,7 +23,9 @@ function runTest ([id, { script, description, twigifyOptions = {} }]) {
     }).bundle((err, output) => {
       assert.ifError(err)
 
+      // eslint-disable-next-line prefer-const
       let html = ''
+      // eslint-disable-next-line no-eval
       eval(output.toString())
 
       assert.strictEqual(html.trim(), expectedResult.trim())
@@ -32,87 +34,3 @@ function runTest ([id, { script, description, twigifyOptions = {} }]) {
     })
   })
 }
-/*
-
-tap.test('template generation with custom filter, include & extend', childTest => {
-  const testScript = `const Twig = require('twig')
-                      const main = require('./test/resources/1_main.twig')
-                      require('./test/resources/includes/header.twig')
-                      require('./test/resources/includes/footer.twig')
-                      require('./test/resources/includes/layout.twig')
-
-                      Twig.extendFilter('custom_uppercase', str => str.toUpperCase())
-
-                      html = main.render({
-                        pageName: 'twigify',
-                        greeting: 'Hello, World!'
-                      })`
-
-  browserify(testScript, {
-    transform: [twigify]
-  }).bundle((err, output) => {
-    assert.ifError(err)
-
-    // eslint-disable-next-line prefer-const
-    let html = ''
-    // eslint-disable-next-line no-eval
-    eval(output.toString())
-
-    assert.strictEqual(html.trim(), '<html>\n' +
-      '<head>\n' +
-      '    <title>This is a test</title>\n' +
-      '        <meta charset="utf-8">\n' +
-      '</head>\n' +
-      '<body>\n' +
-      '<h1>TWIGIFY</h1>\n' +
-      '<section>    <h1>Welcome to this test!</h1>\n' +
-      '</section>\n' +
-      '<footer>Hello, World!</footer>\n' +
-      '</body>\n' +
-      '</html>')
-
-    childTest.end()
-  })
-})
-
-tap.test('passing a custom extension should work', childTest => {
-  const testScript = `const Twig = require('twig')
-                      const main = require('./test/resources/2_main.tpl')
-                      require('./test/resources/includes/header.twig')
-                      require('./test/resources/includes/footer.twig')
-                      require('./test/resources/includes/layout.twig')
-
-                      Twig.extendFilter('custom_uppercase', str => str.toUpperCase())
-
-                      html = main.render({
-                        pageName: 'twigify',
-                        greeting: 'Hello, World!'
-                      })`
-
-  browserify(testScript, {
-    transform: [[twigify, { ext: /\.(twig|tpl)$/ }]]
-  }).bundle((err, output) => {
-    assert.ifError(err)
-
-    // eslint-disable-next-line prefer-const
-    let html = ''
-    // eslint-disable-next-line no-eval
-    eval(output.toString())
-
-    assert.strictEqual(html.trim(), '<html>\n' +
-      '<head>\n' +
-      '    <title>This is a test</title>\n' +
-      '        <meta charset="utf-8">\n' +
-      '</head>\n' +
-      '<body>\n' +
-      '<h1>TWIGIFY</h1>\n' +
-      '<section>    <h1>Welcome to this test!</h1>\n' +
-      '</section>\n' +
-      '<footer>Hello, World!</footer>\n' +
-      '</body>\n' +
-      '</html>', 'Generated string should be valid')
-
-    childTest.end()
-  })
-})
-*/
